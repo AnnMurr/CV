0d2cdb0e4fba7b14fb3b9ecad180f354a9ae617e
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2023-11-02 17:22:52 +0200

    added certifications section

--- src/app/core/skills.ts
+++ src/app/core/skills.ts
@@ -1,4 +1,4 @@
-import { LIST_ELEMENTS, DETAILS_ELEMENTS_DATA } from "../consts/options"
+import { LIST_ELEMENTS, DETAILS_ELEMENTS_DATA, SERTIFICATIONS_DATA } from "../consts/options"
 import { createDubleLine } from "./helpers/createDubleLine"
 import { createHeading } from "./helpers/createHeaading"
 
@@ -7,7 +7,7 @@ const infoWrapper: HTMLElement | null = document.querySelector('.info__wrapper')
 export function createSkilsWrapper(): void {
     const skills: HTMLElement = document.createElement('div')
     skills.classList.add('skills')
-    skills.append(createHeading('Skills'), createDubleLine(), createSkillsList(), createDetailsWrapper(), createDetailsItemsWrapper())
+    skills.append(createHeading('Skills'), createDubleLine(), createSkillsList(), createDetailsWrapper(), createDetailsItemsWrapper(), createCertificationsWrapper())
     infoWrapper?.append(skills)
 }
 
@@ -20,7 +20,7 @@ function createSkillsList(): HTMLUListElement {
 }
 
 function createListElement(list: HTMLUListElement): void {
-    const slillsList:HTMLUListElement = list
+    const slillsList: HTMLUListElement = list
 
     LIST_ELEMENTS.forEach(option => {
         const listItem: HTMLLIElement = document.createElement('li')
@@ -29,19 +29,19 @@ function createListElement(list: HTMLUListElement): void {
 
         option.title && option.title.length > 0 && (listTitle.textContent = option.title)
 
-        if(option.class && Array.isArray(option.class)) {
+        if (option.class && Array.isArray(option.class)) {
             option.class.forEach(element => {
                 progressBar.classList.add(element)
             })
         }
-    
-        if(option.attributes && Array.isArray(option.attributes)){
-           option.attributes.forEach(attribute => {
-            const [key, value] = Object.entries(attribute)[0]
-            progressBar.setAttribute(key, value)
-           })
+
+        if (option.attributes && Array.isArray(option.attributes)) {
+            option.attributes.forEach(attribute => {
+                const [key, value] = Object.entries(attribute)[0]
+                progressBar.setAttribute(key, value)
+            })
         }
-       
+
         listItem.append(listTitle, progressBar)
         slillsList.appendChild(listItem)
     })
@@ -57,7 +57,7 @@ function createDetailsWrapper(): HTMLDivElement {
 
 function createDetailsItemsWrapper(): HTMLDivElement {
     const items: HTMLDivElement = document.createElement('div')
-    items.classList.add('skills__details-items') 
+    items.classList.add('skills__details-items')
     createDetailsItem(items)
 
     return items
@@ -72,10 +72,10 @@ function createDetailsItem(items: HTMLDivElement): void {
         const itemTitleText: HTMLHeadingElement = document.createElement('h4')
         const itemList: HTMLUListElement = document.createElement('ul')
         itemList.classList.add('details-item__text')
-        
+
         option.title && (itemTitleText.textContent = option.title)
 
-        if(option.text && Array.isArray(option.text)) {
+        if (option.text && Array.isArray(option.text)) {
             option.text.forEach(item => {
                 const itemListText: HTMLLIElement = document.createElement('li')
                 itemListText.textContent = item
@@ -87,4 +87,85 @@ function createDetailsItem(items: HTMLDivElement): void {
         item.append(itemTitle, itemList)
         items.append(item)
     })
+}
+
+function createCertificationsWrapper(): HTMLDivElement {
+    const certifications: HTMLDivElement = document.createElement('div')
+    certifications.classList.add('skills__certifications')
+    certifications.append(createHeading('Licenses & Certifications'), createDubleLine(), createBloksWrap())
+
+    return certifications
+}
+
+function createBloksWrap() : HTMLDivElement {
+    const blocks: HTMLDivElement = document.createElement('div')
+    blocks.classList.add('certifications__blocks')
+    createBlockCertifications(blocks)
+
+    return blocks
+}
+
+function createBlockCertifications(wrap): void {
+    const wrapper: HTMLDivElement = wrap
+
+    SERTIFICATIONS_DATA.forEach((item) => {
+        const blockCertifications: HTMLDivElement = document.createElement('div')
+        blockCertifications.classList.add('certifications-block')
+        const list = createBlockListCertifications()
+
+        list.append(createBlockTitle(item.title),
+            createBlockSubitle(item.subTitle),
+            createBlockLink(item.link, item.textLink))
+
+        blockCertifications.append(createBlockIcon(item.icon), list)
+        wrapper.append(blockCertifications)
+    })
+}
+
+function createBlockListCertifications(): HTMLDivElement {
+    const wrap: HTMLDivElement = document.createElement('div')
+    wrap.classList.add('certifications-block-list')
+
+    return wrap
+}
+
+function createBlockTitle(text): HTMLDivElement {
+    const inner: HTMLDivElement = document.createElement('div')
+    const title: HTMLHeadElement = document.createElement('h4')
+    inner.classList.add('certifications-block__title')
+    title.textContent = text
+    inner.append(title)
+
+    return inner
+}
+
+function createBlockSubitle(text): HTMLDivElement {
+    const inner: HTMLDivElement = document.createElement('div')
+    const subTitle: HTMLHeadElement = document.createElement('h5')
+    inner.classList.add('certifications-block__subTitle')
+    subTitle.textContent = text
+    inner.append(subTitle)
+
+    return inner
+}
+
+function createBlockLink(value, text): HTMLDivElement {
+    const inner: HTMLDivElement = document.createElement('div')
+    const link: HTMLAnchorElement = document.createElement('a')
+    inner.classList.add('certifications-block__link')
+    link.text = text
+    link.href = value
+    inner.append(link)
+
+    return inner
+}
+
+function createBlockIcon(link): HTMLDivElement {
+    const inner: HTMLDivElement = document.createElement('div')
+    const image: HTMLImageElement = document.createElement('img')
+    inner.classList.add('certifications-block__image')
+    image.src = link
+    inner.append(image)
+    
+    return inner
 }
